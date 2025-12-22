9975033c240e207898374e9840ab78c7ad09eb54
Vinay Vamshee <vinayvamshee@Vinays-MacBook-Air.local>
2025-12-22 19:31:01 +0530

    update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://project-abc-server.vercel.app//api",
+  baseURL: "https://project-abc-server.vercel.app/api",
   withCredentials: true,
 });
 
