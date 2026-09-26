b817e5bf9e84369f4ad69eeba6eff8047fbc4503
Vinay Vamshee <vinayvamshee2183@gmail.com>
2026-09-26 23:13:53 +0530

    update

--- client/src/pages/Ledger/LedgerDashboard.js
+++ client/src/pages/Ledger/LedgerDashboard.js
@@ -300,6 +300,7 @@ export default function LedgerDashboard() {
   useEffect(() => {
     if (activeTab === "transactions") loadTransactions();
     if (activeTab === "obligations") loadObligations();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeTab]);
 
   // Auto-calculate gold valuation
